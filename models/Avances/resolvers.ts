0604837305dc092637e5f3ae694ffa4696cb2690
David Andres Gonzalez Medina <davico.gonzalez@live.com>
2021-11-20 13:50:03 -0500

    Corrección métodos para Avances (Query y Mutations).

--- models/Avances/resolvers.ts
+++ models/Avances/resolvers.ts
@@ -3,14 +3,14 @@ import { AvanceModel } from "./avance";
 const resolverAvances = {
     Query: {
         Avances: async (parent, args) => {
-            const avances = await AvanceModel.find();
+            const avances = await AvanceModel.find().populate("proyecto").populate("creadoPor");
             return avances;
         }},
     
     Mutation: {
         crearAvance: async (parent, args) => {
             const avanceCreado = await AvanceModel.create({
-                fechaAvance: args.fechaAvance,
+                fechaAvance: new Date(args.fechaAvance),
                 descripcion: args.descripcion,
                 proyecto: args.proyecto,
                 creadoPor: args.creadoPor,
