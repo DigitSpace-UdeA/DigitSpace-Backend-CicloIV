d1b54ff5ca3820bc4ce9dd0ba968e5342ed2bc3f
katerin <katerinbarrera2001@gmail.com>
2021-12-15 19:03:30 -0500

    edit en tipos de proyectos

--- models/Proyectos/tipos.js
+++ models/Proyectos/tipos.js
@@ -51,10 +51,6 @@ const tiposProyecto = gql`
     crearProyecto(
       nombre: String!
       presupuesto: Float!
-      fechaInicial: Date!
-      fechaFinal: Date!
-      estadoProyecto: Enum_Estado_Proyecto!
-      faseProyecto: Enum_Fase_Proyecto!
       lider: String!
       objetivos: [crearObjetivo]
     ): Proyecto
@@ -77,3 +73,15 @@ const tiposProyecto = gql`
 `;
 
 export { tiposProyecto };
+
+// type Mutation {
+//   crearProyecto(
+//     nombre: String!
+//     presupuesto: Float!
+//     fechaInicial: Date!
+//     fechaFinal: Date!
+//     estadoProyecto: Enum_Estado_Proyecto!
+//     faseProyecto: Enum_Fase_Proyecto!
+//     lider: String!
+//     objetivos: [crearObjetivo]
+//   ): Proyecto
