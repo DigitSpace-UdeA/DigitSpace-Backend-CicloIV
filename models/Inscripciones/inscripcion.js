ad165cb923ae9be399a997f36024f3c465438b54
Yesica <yesicavillamil@gmail.com>
2021-12-18 01:38:47 -0500

    Revertir cambios de Daniel

--- models/Inscripciones/inscripcion.js
+++ models/Inscripciones/inscripcion.js
@@ -1,4 +1,58 @@
+// import mongoose from "mongoose";
+// import { ProjectModel } from "../Proyectos/proyecto.js";
+// import { UserModel } from "../Usuarios/usuario.js";
+
+// const { Schema, model } = mongoose;
+
+// // interface Inscripcion {
+// //   estado: Enum_Estado_Inscripcion;
+// //   fechaIngreso: Date;
+// //   fechaEgreso: Date;
+// //   proyecto: Schema.Types.ObjectId;
+// //   estudiante: Schema.Types.ObjectId;
+// // }
+
+// const inscripcionSchema =
+//   new Schema(
+//   {
+//     estadoInscripcion: {
+//       type: String,
+//       enum: ["ACEPTADA", "RECHAZADA", "PENDIENTE"],
+//       default: "PENDIENTE",
+//       required: true,
+//     },
+//     fechaIngreso: {
+//       type: Date,
+//       required: false,
+//     },
+//     fechaEgreso: {
+//       type: Date,
+//       required: false,
+//     },
+//     proyecto: {
+//       type: Schema.Types.ObjectId,
+//       ref: ProjectModel,
+//       required: true,
+//     },
+//     estudiante: {
+//       type: Schema.Types.ObjectId,
+//       ref: UserModel,
+//       required: true,
+//     },
+//   });
+
+// const InscripcionModel = model("Inscripcion", inscripcionSchema, "Inscripciones_Proyectos");
+
+// export { InscripcionModel };
+
+
+
+
+
 import mongoose from 'mongoose';
+// import { Enum_EstadoInscripcion } from '../enums/enums.js';
+// import { ProjectModel } from '../proyecto/proyecto.js';
+// import { UserModel } from '../usuarios/usuario.js';
 import { ProjectModel } from "../Proyectos/proyecto.js";
 import { UserModel } from "../Usuarios/usuario.js";
 
--- models/Inscripciones/tipos.js
+++ models/Inscripciones/tipos.js
@@ -6,7 +6,7 @@ const tiposInscripcion = gql`
     estadoInscripcion: Enum_Estado_Inscripcion!
     fechaIngreso: Date
     fechaEgreso: Date
-    proyecto(lider: String): Proyecto
+    proyecto: Proyecto!
     estudiante: Usuario!
   }
 
@@ -16,6 +16,7 @@ const tiposInscripcion = gql`
 
   type Mutation {
     crearInscripcion(
+      estadoInscripcion: Enum_Estado_Inscripcion!
       proyecto: String!
       estudiante: String!
     ): Inscripcion
@@ -25,3 +26,25 @@ const tiposInscripcion = gql`
 `;
 
 export { tiposInscripcion };
+
+// import { gql } from 'apollo-server-express';
+
+// const tiposInscripcion = gql`
+//   type Inscripcion {
+//     _id: ID!
+//     estado: Enum_EstadoInscripcion!
+//     fechaIngreso: Date
+//     fechaEgreso: Date
+//     proyecto(lider: String): Proyecto
+//     estudiante: Usuario!
+//   }
+//   type Query {
+//     Inscripciones: [Inscripcion]
+//   }
+//   type Mutation {
+//     crearInscripcion(proyecto: String!, estudiante: String!): Inscripcion
+//     aprobarInscripcion(id: String!): Inscripcion
+//   }
+// `;
+
+// export { tiposInscripcion };
